e83c56421cc40dcf5283c4c3e1220588aa8db8ef
BenjaminPellieux <benjaminpellieux5@gmail.com>
2024-06-17 12:49:13 +0200

    Iced interface graphique

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,46 @@
+use iced::{button, Button, Column, Element, Sandbox, Settings, Text};
+
+#[derive(Default)]
+struct HelloWorld {
+    button: button::State,
+    message: String,
+}
+
+#[derive(Debug, Clone)]
+enum Message {
+    ButtonPressed,
+}
+
+impl Sandbox for HelloWorld {
+    type Message = Message;
+
+    fn new() -> Self {
+        Self::default()
+    }
+
+    fn title(&self) -> String {
+        String::from("Hello Iced")
+    }
+
+    fn update(&mut self, message: Message) {
+        match message {
+            Message::ButtonPressed => {
+                self.message = String::from("Hello, world!");
+            }
+        }
+    }
+
+    fn view(&mut self) -> Element<Message> {
+        Column::new()
+            .push(
+                Button::new(&mut self.button, Text::new("Press me"))
+                    .on_press(Message::ButtonPressed),
+            )
+            .push(Text::new(&self.message))
+            .into()
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    HelloWorld::run(Settings::default());
 }
